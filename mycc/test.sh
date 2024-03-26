db5b4b57e69dba3682b0fab3d68e031f0c9c4c24
mathatomo57 <akagawa.code@gmail.com>
2024-03-26 21:26:36 +0900

    ステップ７まで実装

--- mycc/test.sh
+++ mycc/test.sh
@@ -31,4 +31,24 @@ assert 4 "(3+5)/2"
 assert 5 "+3-(-2)"
 assert 12 "3*(-5) ++27"
 
+assert 0 "0==1"
+assert 1 "42==42"
+assert 1 "0!=1"
+assert 0 "42!=42"
+
+assert 1 "0<1"
+assert 0 "1<1"
+assert 0 "2<1"
+assert 1 "0<=1"
+assert 1 "1<=1"
+assert 0 "2<=1"
+
+
+assert 1 "1>0"
+assert 0 "1>1"
+assert 0 "1>2"
+assert 1 "1>=0"
+assert 1 "1>=1"
+assert 0 "1>=2"
+
 echo OK
--- mycc/tmp.s
+++ mycc/tmp.s
@@ -1,21 +1,13 @@
 .intel_syntax noprefix
 .global main
 main:
-	push 3
-	push 0
-	push 5
+	push 2
+	push 1
 	pop rdi
 	pop rax
-	sub rax, rdi
-	push rax
-	pop rdi
-	pop rax
-	imul rax, rdi
-	push rax
-	push 27
-	pop rdi
-	pop rax
-	add rax, rdi
+	cmp rax, rdi
+	setle al
+	movzb rax, al
 	push rax
 	pop rax
 	ret
